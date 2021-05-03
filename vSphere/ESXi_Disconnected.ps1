9062b2a63baeda816487e91effde12a1910516f4
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-05-03 21:35:05 +0100

    Create ESXi_Disconnected.ps1

---
+++ vSphere/ESXi_Disconnected.ps1
@@ -0,0 +1,33 @@
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+$vCenterCreds = Import-clixml -path /home/jenkins/workspace/imports/cred.cred
+
+$vCenter_Servers = 
+$Disconnected = @() #Array
+
+
+foreach($vCenter in $vCenter_Servers) {
+Connect-VIServer -Server $vCenter -Credential $vCenterCreds
+$esx = Get-VMHost -PipelineVariable esx -Server $vCenter |  Where-Object {$_.ConnectionState -eq 'Disconnected'} 
+$Disconnected += $esx | Select-Object -ExpandProperty Name
+
+foreach($CreateTicket in $Disconnected) {
+$CreateTicket
+
+$SMTPBODY =
+@"
+    Hi Team,
+    ESXi Checks have Found That the Following ESXi Host Machine Seems to have a Status of Disconnected:
+    ESXi Host: $CreateTicket
+    vCenter: $vCenter
+    Can we Please verify the Status of This ESXi Host.
+    Thanks.
+"@
+
+Send-MailMessage -SmtpServer 'smtphost.qualcomm.com' -From  -To  -Cc  -Subject 'ESXi Host in Disconnected State' -Body $SMTPBody
+
+    }
+
+$Disconnected = @() #Array
+Disconnect-VIServer -Server $vCenter -Confirm:$False
+
+}
