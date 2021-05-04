e9691edb4b89a5de8d2eada670589e2572bec395
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-05-04 09:02:31 +0100

    Create RemoveUser.ps1

---
+++ Random/RemoveUser.ps1
@@ -0,0 +1,43 @@
+  
+<#
+Once Database has being Restored, update HOSTNAME_NAME and IP Address & FTP Listner 
+for the secondary SFTP Server. 
+#>
+
+PARAM ()
+BEGIN{
+#Global Variables
+$smtpServer=  #SMTP SERVER
+$from= #Email From
+$testing="Disabled"#Disabled to Email Users
+$testRecipient=#Testing Email
+$support=#Email VGTSI Support 
+$date = Get-Date -format MMddyyyy # Get Date 
+
+    # modify these to point to your db server and database
+    $SqlServer =  #DB Server
+    $SqlDBName =  #Database being Connected to
+    # use these for a sql login.  Or you can modify the connection string to use integrated authentication and set the scheduled task to run under that user context (omit User ID and Password from the connect string).
+    $SqlLoginUser = #Username
+    $SqlLoginPassword = #Password 
+
+    #Database Connection and Query
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = "Server=$SqlServer;Database=$SqlDBName;Integrated Security=SSPI;Persist Security Info=False;User ID=$SqlLoginUser;Password=$SqlLoginPassword"
+    $SqlQuery="UPDATE dbo.FTP_Listeners
+				 SET IP_Address=''
+				 WHERE IP_Address=';
+         UPDATE dbo.Hosts
+				 SET Host_Name='SECONDARY.local.com', Top_Dir='C:\Program Files (x86)\Ipswitch\WS_FTP Server\SECONDARY.local.com'
+				 WHERE Host_Name='PRIMARY.local.com';"
+
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.CommandText = $SqlQuery
+    $SqlCmd.Connection = $SqlConnection
+    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+    $SqlAdapter.SelectCommand = $SqlCmd
+    $DataSet = New-Object System.Data.DataSet
+    [void] $SqlAdapter.Fill($DataSet)
+    $SqlConnection.Close() # Close Connection to DB
+}
+END{}
